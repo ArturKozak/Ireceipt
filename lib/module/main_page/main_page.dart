6cafae43b5060c4b30efb68f073504e6823c11fd
Artur Kozak <kozak@fedandburk.com>
2023-04-25 10:28:35 +0200

    Remove the main animation cubit

--- lib/module/main_page/main_page.dart
+++ lib/module/main_page/main_page.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ireceipt/module/camera_page/cubit/camera_cubit.dart';
-import 'package:ireceipt/module/camera_page/cubit/flashlight_cubit.dart';
-import 'package:ireceipt/module/main_page/cubit/main_animation_cubit.dart';
 import 'package:ireceipt/module/main_page/widgets/bottom_animated_panel.dart';
 import 'package:ireceipt/module/main_page/widgets/main_top_custom_panel.dart';
 import 'package:ireceipt/module/main_page/widgets/main_top_info_panel.dart';
@@ -17,25 +13,12 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => MainAnimationCubit()..init(this),
-        ),
-        BlocProvider(
-          create: (_) => CameraCubit()..initControllers(),
-        ),
-        BlocProvider(
-          create: (_) => FlashlightCubit(),
-        ),
+    return Stack(
+      children: [
+        MainTopCustomPanel(),
+        MainTopInfoPanel(),
+        BottomAnimatedPanel(),
       ],
-      child: Stack(
-        children: [
-          MainTopCustomPanel(),
-          MainTopInfoPanel(),
-          BottomAnimatedPanel(),
-        ],
-      ),
     );
   }
 }
